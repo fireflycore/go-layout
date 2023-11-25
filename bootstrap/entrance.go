d243165efdad7cd69a2923f586c609df4ca55c26
lhdht <15229684090@163.com>
2023-11-25 19:14:05 +0800

    feature: "注册微服务"

--- bootstrap/entrance.go
+++ bootstrap/entrance.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	_ "embed"
 	"microservice-go/core"
+	"microservice-go/register"
 	"microservice-go/store"
 )
 
@@ -15,4 +16,6 @@ func Setup() {
 	store.Use.Micro.Cli = core.Use.DB.SetupEtcd(&store.Use.Config.Local.Micro.RegisterCenter)
 	store.Use.Micro.Ctx, store.Use.Micro.Cancel = context.WithCancel(context.Background())
 	store.Use.Micro.Lease = core.Use.Micro.Etcd.CreateLease(store.Use.Micro.Cli)
+
+	register.ServiceInstance()
 }
---
+++ register/entrance.go
@@ -0,0 +1,31 @@
+package register
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"microservice-go/store"
+	"microservice-go/utils"
+	"net"
+)
+
+func ServiceInstance() {
+	logPrefix := "setup grpc"
+	utils.Use.Logger.Info(fmt.Sprintf("%s %s %s", logPrefix, store.Use.Config.Local.Micro.Address.Inside, "start ->"))
+
+	listen, err := net.Listen("tcp", store.Use.Config.Local.Micro.Address.Inside)
+	if err != nil {
+		utils.Use.Logger.Error(fmt.Sprintf("%s %s", logPrefix, err.Error()))
+		return
+	}
+	server := grpc.NewServer()
+
+	/*-------------------------------------Register Microservice---------------------------------*/
+	/*-------------------------------------Register Microservice---------------------------------*/
+
+	utils.Use.Logger.Info(fmt.Sprintf("%s %s", logPrefix, "register server done ->"))
+	sErr := server.Serve(listen)
+	if sErr != nil {
+		utils.Use.Logger.Error(fmt.Sprintf("%s %s", logPrefix, sErr.Error()))
+		return
+	}
+}
