c72249ad7263924ee4b81091986a0463054baabe
lhdht <15229684090@163.com>
2023-11-25 14:35:41 +0800

    feature: init bootstrap

---
+++ bootstrap/entrance.go
@@ -0,0 +1,18 @@
+package bootstrap
+
+import (
+	"context"
+	_ "embed"
+	"microservice-go/core"
+	"microservice-go/store"
+)
+
+//go:embed file/config.yaml
+var CONFIG []byte
+
+func Setup() {
+	store.Use.Config.Local = core.Use.SetupViper(&CONFIG)
+	store.Use.Micro.Cli = core.Use.DB.SetupEtcd(&store.Use.Config.Local.Micro.RegisterCenter)
+	store.Use.Micro.Ctx, store.Use.Micro.Cancel = context.WithCancel(context.Background())
+	store.Use.Micro.Lease = core.Use.Micro.Etcd.CreateLease(store.Use.Micro.Cli)
+}
