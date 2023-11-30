e2e870a48549f028764e43c637d20cffff796dfd
lhdht <15229684090@163.com>
2023-11-30 14:27:11 +0800

    feature: "注册时间方法集合"

--- utils/entrance.go
+++ utils/entrance.go
@@ -2,10 +2,14 @@ package utils
 
 import (
 	"microservice-go/utils/logger"
+	"microservice-go/utils/mongo"
+	"microservice-go/utils/time"
 )
 
 type _Entrance struct {
 	Logger logger.Entrance
+	Time   time.Entrance
+	Mongo  mongo.Entrance
 }
 
 var Use = new(_Entrance)
