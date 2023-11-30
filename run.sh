3460d122b2df9469f8b4f9af4c75f6d372b18f57
lhdht <15229684090@163.com>
2023-11-30 16:53:42 +0800

    feature: 服务运行shell

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # shellcheck disable=SC2009
-pid=$(ps -ef | grep /opt/app/lhdht-gateway/logger/main | grep -v grep | awk '{print $2}')
+pid=$(ps -ef | grep /opt/app/lhdht-gateway/microservice/main | grep -v grep | awk '{print $2}')
 if [ -n "$pid" ]
 then
    kill -9 "$pid"
 fi
 
 sleep 5
-nohup /opt/app/lhdht-gateway/logger/main > ./nohup.log 2>&1 &
+nohup /opt/app/lhdht-gateway/microservice/main > ./nohup.log 2>&1 &
