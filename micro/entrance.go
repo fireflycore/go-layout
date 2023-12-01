cb8f9f2063200190d4866c2be4e9c9bce8b511b7
lhdht <15229684090@163.com>
2023-12-01 20:18:02 +0800

    feature: 重构

--- micro/entrance.go
+++ micro/entrance.go
@@ -1,31 +1,13 @@
 package micro
 
 import (
-	"fmt"
-	"google.golang.org/grpc"
-	"microservice-go/store"
-	"microservice-go/utils"
-	"net"
+	"microservice-go/micro/etcd"
+	"microservice-go/micro/grpc"
 )
 
-func RegisterServiceInstance() {
-	logPrefix := "setup grpc"
-	utils.Use.Logger.Info(fmt.Sprintf("%s %s %s", logPrefix, store.Use.Config.Local.Micro.Address.Inside, "start ->"))
-
-	listen, err := net.Listen("tcp", store.Use.Config.Local.Micro.Address.Inside)
-	if err != nil {
-		utils.Use.Logger.Error(fmt.Sprintf("%s %s", logPrefix, err.Error()))
-		return
-	}
-	server := grpc.NewServer()
-
-	/*-------------------------------------Register Microservice---------------------------------*/
-	/*-------------------------------------Register Microservice---------------------------------*/
-
-	utils.Use.Logger.Info(fmt.Sprintf("%s %s", logPrefix, "register server done ->"))
-	sErr := server.Serve(listen)
-	if sErr != nil {
-		utils.Use.Logger.Error(fmt.Sprintf("%s %s", logPrefix, sErr.Error()))
-		return
-	}
+type _Entrance struct {
+	Etcd etcd.Entrance
+	Grpc grpc.Entrance
 }
+
+var Use = new(_Entrance)
