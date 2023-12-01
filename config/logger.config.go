9defb1be70312408a491eeeff8d920a675a091c9
lhdht <15229684090@163.com>
2023-12-01 20:14:49 +0800

    fix: 废弃

--- config/logger.config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "microservice-go/model/config"
-
-type _LoggerConfigEntrance struct {
-	Mode   bool                   `json:"mode" yaml:"mode" mapstructure:"mode"`       // Mode is true enabling remote logging, is false console local(zap is available)
-	Enable bool                   `json:"enable" yaml:"enable" mapstructure:"enable"` // Enable is logger
-	Remote *config.DBConfigEntity `json:"remote" yaml:"remote" mapstructure:"remote"` // Remote database config
-}
--- core/micro/entrance.go
+++
@@ -1,7 +0,0 @@
-package micro
-
-import "microservice-go/core/micro/etcd"
-
-type Entrance struct {
-	Etcd etcd.Entrance
-}
