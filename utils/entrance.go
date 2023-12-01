7d5e6589d936de97b4d2cf0d1e2a7a94ce1bc129
lhdht <15229684090@163.com>
2023-12-01 20:04:48 +0800

    fix: "废弃日志节点"

--- utils/entrance.go
+++ utils/entrance.go
@@ -1,15 +1,13 @@
 package utils
 
 import (
-	"microservice-go/utils/logger"
 	"microservice-go/utils/mongo"
 	"microservice-go/utils/time"
 )
 
 type _Entrance struct {
-	Logger logger.Entrance
-	Time   time.Entrance
-	Mongo  mongo.Entrance
+	Time  time.Entrance
+	Mongo mongo.Entrance
 }
 
 var Use = new(_Entrance)
