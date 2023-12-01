03a814276e097678cd321ad2f91346953cc1976d
lhdht <15229684090@163.com>
2023-12-01 20:04:34 +0800

    feature: "重构日志方法"

--- utils/logger/entrance.go
+++ utils/logger/entrance.go
@@ -1,26 +1,71 @@
 package logger
 
-import "microservice-go/utils/logger/internal"
+import (
+	"microservice-go/utils/logger/internal"
+)
 
-type Entrance struct {
+type Interface interface {
+	Info(message string)
+	Success(message string)
+	Warning(message string)
+	Error(message string)
 }
 
-// Info The normal log
-func (Entrance) Info(log string) {
-	internal.Logger("info", log)
+func New(config ConfigEntity) Interface {
+	if len(config.Dictionary) != 4 {
+		config.Dictionary = []uint32{10, 11, 12, 13}
+	}
+	return &entrance{config}
 }
 
-// Success The result is a success log
-func (Entrance) Success(log string) {
-	internal.Logger("success", log)
+type ConfigEntity struct {
+	Enable     bool     `json:"enable" bson:"enable" yaml:"enable" mapstructure:"enable"`                 // Enable is remote
+	Console    bool     `json:"console" bson:"console" yaml:"console" mapstructure:"console"`             // Console is console
+	Dictionary []uint32 `json:"dictionary" bson:"dictionary" yaml:"dictionary" mapstructure:"dictionary"` // Dictionary remote storage dictionary code
+	WithRemote func(level uint32, message string)
 }
 
-// Warning The result is abnormal but does not affect running logs
-func (Entrance) Warning(log string) {
-	internal.Logger("warning", log)
+type entrance struct {
+	Config ConfigEntity
 }
 
-// Error The result is abnormal and affects program operation
-func (Entrance) Error(log string) {
-	internal.Logger("error", log)
+func (c *entrance) Info(message string) {
+	internal.Logger(&internal.ConfigEntity{
+		Console: c.Config.Console,
+		Enable:  c.Config.Enable,
+		Ltd:     c.Config.Dictionary[0],
+		Level:   "info",
+		Message: message,
+		Remote:  c.Config.WithRemote,
+	})
+}
+func (c *entrance) Success(message string) {
+	internal.Logger(&internal.ConfigEntity{
+		Console: c.Config.Console,
+		Enable:  c.Config.Enable,
+		Ltd:     c.Config.Dictionary[1],
+		Level:   "success",
+		Message: message,
+		Remote:  c.Config.WithRemote,
+	})
+}
+func (c *entrance) Warning(message string) {
+	internal.Logger(&internal.ConfigEntity{
+		Console: c.Config.Console,
+		Enable:  c.Config.Enable,
+		Ltd:     c.Config.Dictionary[2],
+		Level:   "warning",
+		Message: message,
+		Remote:  c.Config.WithRemote,
+	})
+}
+func (c *entrance) Error(message string) {
+	internal.Logger(&internal.ConfigEntity{
+		Console: c.Config.Console,
+		Enable:  c.Config.Enable,
+		Ltd:     c.Config.Dictionary[3],
+		Level:   "error",
+		Message: message,
+		Remote:  c.Config.WithRemote,
+	})
 }
--- utils/logger/internal/logger.go
+++ utils/logger/internal/logger.go
@@ -5,6 +5,21 @@ import (
 	"time"
 )
 
-func Logger(level string, log string) {
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), level, log)
+type ConfigEntity struct {
+	Console bool
+	Enable  bool
+	Ltd     uint32
+	Level   string
+	Message string
+	Remote  func(level uint32, message string)
+}
+
+func Logger(config *ConfigEntity) {
+	if config.Console {
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), config.Level, config.Message)
+	}
+
+	if config.Enable && config.Remote != nil {
+		config.Remote(config.Ltd, config.Message)
+	}
 }
