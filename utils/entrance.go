e0f2cfbbce5cfecd65acd7f9b51bb9c1f6f930ba
lhdht <15229684090@163.com>
2023-12-07 16:37:32 +0800

    feature: "结构体转换函数"

--- utils/entrance.go
+++ utils/entrance.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"microservice-go/utils/mongo"
 	"microservice-go/utils/time"
 )
@@ -11,3 +13,15 @@ type _Entrance struct {
 }
 
 var Use = new(_Entrance)
+
+func StructConvert[S any, T any](source *S, target *T) {
+	marshal, mErr := json.Marshal(source)
+	if mErr != nil {
+		fmt.Println(mErr.Error())
+		return
+	}
+	if err := json.Unmarshal(marshal, &target); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
