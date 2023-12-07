010c8f3931877222e0bf25ff92d6f1c65ed87597
lhdht <15229684090@163.com>
2023-12-07 17:52:17 +0800

    feature: 部署配置

--- config/micro.config.go
+++ config/micro.config.go
@@ -6,6 +6,7 @@ import (
 )
 
 type _MicroConfigEntrance struct {
+	Deploy         string                `json:"deploy" yaml:"deploy" mapstructure:"deploy"`                   // Deploy mode (LAN local area network) (NAN not area network)
 	GatewayName    string                `json:"gateway_name" yaml:"gateway_name" mapstructure:"gateway_name"` // GatewayName GetawayName The gateway name is used to bind the gateway when registering microservices, facilitating gateway service discovery
 	Namespace      string                `json:"namespace" yaml:"namespace" mapstructure:"namespace"`
 	Address        base.NetworkAddress   `json:"address" yaml:"address" yaml:"address" mapstructure:"address"`
