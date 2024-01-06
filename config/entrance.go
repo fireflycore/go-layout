e240fc41d9954aa887e639449262d463e7091bdf
lhdht <15229684090@163.com>
2024-01-06 19:14:03 +0800

    perfect: "系统配置注册和grpc注册"

--- config/entrance.go
+++ config/entrance.go
@@ -3,9 +3,12 @@ package config
 import (
 	"github.com/lhdhtrc/microservice-go/logger"
 	"github.com/lhdhtrc/microservice-go/micro"
+	"github.com/lhdhtrc/microservice-go/micro/grpc"
 )
 
-type Entrance struct {
+type EntranceEntity struct {
+	System SystemConfigEntity  `json:"system" bson:"system" yaml:"system" mapstructure:"system"`
+	Grpc   grpc.ConfigEntity   `json:"grpc" bson:"grpc" yaml:"grpc" mapstructure:"grpc"`
 	Logger logger.ConfigEntity `json:"logger" bson:"logger" yaml:"logger" mapstructure:"logger"`
 	Micro  micro.ConfigEntity  `json:"micro" bson:"micro" yaml:"micro" mapstructure:"micro"`
 }
