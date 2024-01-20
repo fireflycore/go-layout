d484dd674ee2cc234505b77337417e57b2659df5
lhdht <15229684090@163.com>
2024-01-20 21:14:46 +0800

    fix: 废弃

--- service/entrance.go
+++ service/entrance.go
@@ -1,12 +1,10 @@
 package service
 
 import (
-	"microservice-go/service/db"
 	"microservice-go/service/logger"
 )
 
 type _Entrance struct {
-	DB     db.EntranceEntity
 	Logger logger.EntranceEntity
 }
 
