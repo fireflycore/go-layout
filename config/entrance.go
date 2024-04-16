01fe65b7ff9531c9dfcb3824dfae9b0da8260cfe
lhdht <15229684090@163.com>
2024-04-16 09:22:05 +0800

    update:config

--- config/entrance.go
+++ config/entrance.go
@@ -1,14 +1,14 @@
 package config
 
 import (
+	loggerModel "github.com/lhdhtrc/logger-go/model"
+	"github.com/lhdhtrc/microcore-go/grpc"
 	microModel "github.com/lhdhtrc/microcore-go/model"
-	"github.com/lhdhtrc/microservice-go/logger"
-	"github.com/lhdhtrc/microservice-go/micro/grpc"
 )
 
 type EntranceEntity struct {
-	System SystemConfigEntity      `json:"system" bson:"system" yaml:"system" mapstructure:"system"`
-	Grpc   grpc.ConfigEntity       `json:"grpc" bson:"grpc" yaml:"grpc" mapstructure:"grpc"`
-	Logger logger.ConfigEntity     `json:"logger" bson:"logger" yaml:"logger" mapstructure:"logger"`
-	Micro  microModel.ConfigEntity `json:"micro" bson:"micro" yaml:"micro" mapstructure:"micro"`
+	System SystemConfigEntity       `json:"system" bson:"system" yaml:"system" mapstructure:"system"`
+	Grpc   grpc.ConfigEntity        `json:"grpc" bson:"grpc" yaml:"grpc" mapstructure:"grpc"`
+	Logger loggerModel.ConfigEntity `json:"logger" bson:"logger" yaml:"logger" mapstructure:"logger"`
+	Micro  microModel.ConfigEntity  `json:"micro" bson:"micro" yaml:"micro" mapstructure:"micro"`
 }
