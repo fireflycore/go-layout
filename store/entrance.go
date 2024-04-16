dc02af3c04642f356a66e903bec42eb4a3f441b2
lhdht <15229684090@163.com>
2024-04-16 09:21:40 +0800

    update:store

--- store/entrance.go
+++ store/entrance.go
@@ -3,8 +3,6 @@ package store
 import (
 	"github.com/lhdhtrc/microcore-go/grpc"
 	microModel "github.com/lhdhtrc/microcore-go/model"
-	"github.com/lhdhtrc/microservice-go/logger"
-
 	taskCore "github.com/lhdhtrc/task-go/core"
 	"go.uber.org/zap"
 	"microservice-go/config"
@@ -13,9 +11,8 @@ import (
 type _Entrance struct {
 	Config       *config.EntranceEntity
 	Logger       *zap.Logger
-	LoggerTemp   []logger.Entity
-	Micro        microModel.Abstraction
-	Grpc         *grpc.EntranceEntity
+	Micro        microModel.MicroCoreInterface
+	Grpc         *grpc.CoreEntity
 	GrpcEndpoint map[string][]string
 
 	Task *taskCore.TaskCoreEntity
