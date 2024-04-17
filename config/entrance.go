dbeea4cdc6d2f538661595382d12e277814da162
lhdht <15229684090@163.com>
2024-04-17 10:08:47 +0800

    fix: 废弃grpc配置

--- config/entrance.go
+++ config/entrance.go
@@ -2,13 +2,11 @@ package config
 
 import (
 	loggerModel "github.com/lhdhtrc/logger-go/model"
-	"github.com/lhdhtrc/microcore-go/grpc"
 	microModel "github.com/lhdhtrc/microcore-go/model"
 )
 
 type EntranceEntity struct {
 	System SystemConfigEntity       `json:"system" bson:"system" yaml:"system" mapstructure:"system"`
-	Grpc   grpc.ConfigEntity        `json:"grpc" bson:"grpc" yaml:"grpc" mapstructure:"grpc"`
 	Logger loggerModel.ConfigEntity `json:"logger" bson:"logger" yaml:"logger" mapstructure:"logger"`
 	Micro  microModel.ConfigEntity  `json:"micro" bson:"micro" yaml:"micro" mapstructure:"micro"`
 }
