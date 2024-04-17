8a8232e1c9bff0f27a926ed17c628b2349e270cf
lhdht <15229684090@163.com>
2024-04-17 22:19:52 +0800

    fix: 修改部署模式

--- config/system.config.go
+++ config/system.config.go
@@ -1,8 +1,7 @@
 package config
 
 type SystemConfigEntity struct {
-	AppId         string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
-	RunPort       string `json:"run_port" bson:"run_port" yaml:"run_port" mapstructure:"run_port"`
-	Deploy        bool   `json:"deploy" bson:"deploy" yaml:"deploy" mapstructure:"deploy"`
-	DeployAddress bool   `json:"deploy_address" bson:"deploy_address" yaml:"deploy_address" mapstructure:"deploy_address"` // outside:inside
+	AppId   string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
+	RunPort string `json:"run_port" bson:"run_port" yaml:"run_port" mapstructure:"run_port"`
+	Deploy  string `json:"deploy" bson:"deploy" yaml:"deploy" mapstructure:"deploy"`
 }
