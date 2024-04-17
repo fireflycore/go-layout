aeea846b49deea532d11aec0074d17f09aa7cfb7
lhdht <15229684090@163.com>
2024-04-17 18:38:48 +0800

    update:pkg

--- config/system.config.go
+++ config/system.config.go
@@ -1,6 +1,8 @@
 package config
 
 type SystemConfigEntity struct {
-	AppId   string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
-	RunPort string `json:"run_port" bson:"run_port" yaml:"run_port" mapstructure:"run_port"`
+	AppId         string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
+	RunPort       string `json:"run_port" bson:"run_port" yaml:"run_port" mapstructure:"run_port"`
+	Deploy        bool   `json:"deploy" bson:"deploy" yaml:"deploy" mapstructure:"deploy"`
+	DeployAddress bool   `json:"deploy_address" bson:"deploy_address" yaml:"deploy_address" mapstructure:"deploy_address"` // outside:inside
 }
