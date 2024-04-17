ef90ad2bbc4f5066f7c833c0eb8274ea273d972e
lhdht <15229684090@163.com>
2024-04-17 22:23:41 +0800

    fix: 迁移到micro.address

--- config/system.config.go
+++ config/system.config.go
@@ -3,5 +3,4 @@ package config
 type SystemConfigEntity struct {
 	AppId   string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
 	RunPort string `json:"run_port" bson:"run_port" yaml:"run_port" mapstructure:"run_port"`
-	Deploy  string `json:"deploy" bson:"deploy" yaml:"deploy" mapstructure:"deploy"`
 }
