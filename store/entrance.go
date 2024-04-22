d49235e984f2720f47692c5f1b986fdbf69cf948
lhdht <15229684090@163.com>
2024-04-22 09:28:19 +0800

    feature: 远程日志服务

--- store/entrance.go
+++ store/entrance.go
@@ -6,14 +6,16 @@ import (
 	taskCore "github.com/lhdhtrc/task-go/core"
 	"go.uber.org/zap"
 	"microservice-go/config"
+	"microservice-go/model"
 )
 
 type _Entrance struct {
-	Config       *config.EntranceEntity
-	Logger       *zap.Logger
-	Micro        microModel.MicroCoreInterface
-	Grpc         *grpc.CoreEntity
-	GrpcEndpoint map[string][]string
+	Config        *config.EntranceEntity
+	Logger        *zap.Logger
+	Micro         microModel.MicroCoreInterface
+	Grpc          *grpc.CoreEntity
+	GrpcEndpoint  map[string][]string
+	RemoteService *model.RemoteServiceEntity
 
 	Task *taskCore.TaskCoreEntity
 }
