905abd16e45fdcce22d1d744eb2bd2fb82039c8f
lhdht <15229684090@163.com>
2024-04-22 09:20:34 +0800

    update:config

--- config/system.config.go
+++ config/system.config.go
@@ -1,6 +1,7 @@
 package config
 
 type SystemConfigEntity struct {
-	AppId   string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
-	RunPort string `json:"run_port" bson:"run_port" yaml:"run_port" mapstructure:"run_port"`
+	AppId         string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
+	RunPort       string `json:"run_port" bson:"run_port" yaml:"run_port" mapstructure:"run_port"`
+	RemoteService string `json:"remote_service" bson:"remote_service" yaml:"remote_service" mapstructure:"remote_service"`
 }
