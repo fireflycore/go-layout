d5abecee2419105692a1a863ff773ad61f40bfc0
lhdht <15229684090@163.com>
2024-07-19 09:04:38 +0800

    update:优化语义

--- config/core.go
+++ config/core.go
@@ -2,11 +2,11 @@ package config
 
 import (
 	loger "github.com/lhdhtrc/logger-go/pkg"
-	tpg "github.com/lhdhtrc/task-go/pkg"
+	task "github.com/lhdhtrc/task-go/pkg"
 )
 
 type CoreEntity struct {
 	System SystemConfigEntity `json:"system" bson:"system" yaml:"system" mapstructure:"system"`
 	Logger loger.ConfigEntity `json:"logger" bson:"logger" yaml:"logger" mapstructure:"logger"`
-	Task   tpg.ConfigEntity   `json:"task" bson:"task" yaml:"task" mapstructure:"task"`
+	Task   task.ConfigEntity  `json:"task" bson:"task" yaml:"task" mapstructure:"task"`
 }
