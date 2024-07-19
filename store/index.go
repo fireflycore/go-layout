7d83f949079a171fa3616aee512e0c10fbe59237
lhdht <15229684090@163.com>
2024-07-19 08:40:08 +0800

    update:store

--- store/index.go
+++ store/index.go
@@ -2,22 +2,23 @@ package store
 
 import (
 	etcd "github.com/lhdhtrc/etcd-go/pkg"
-	task "github.com/lhdhtrc/task-go/pkg"
+	micro "github.com/lhdhtrc/micro-go/pkg"
+	tpg "github.com/lhdhtrc/task-go/pkg"
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
 	"microservice-go/config"
 	"microservice-go/model"
 )
 
 type _StoreEntity struct {
-	Config          *config.EntranceEntity
-	Logger          *zap.Logger
-	Etcd            *etcd.CoreEntity
-	GrpcServer      *grpc.Server
+	Config *config.CoreEntity
+	Logger *zap.Logger
+	Etcd   *etcd.CoreEntity
+	Micro  *micro.CoreEntity
+
 	ServiceDiscover map[string][]string
 	RemoteService   *model.RemoteServiceEntity
 
-	Task *task.CoreEntity
+	Task *tpg.CoreEntity
 }
 
 var Use = new(_StoreEntity)
