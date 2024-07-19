2f4a0c5266b2c3d6b1774e0f12dc92c5a15a2213
lhdht <15229684090@163.com>
2024-07-19 09:02:35 +0800

    update:优化语义

--- store/core.go
+++ store/core.go
@@ -3,22 +3,21 @@ package store
 import (
 	etcd "github.com/lhdhtrc/etcd-go/pkg"
 	micro "github.com/lhdhtrc/micro-go/pkg"
-	tpg "github.com/lhdhtrc/task-go/pkg"
+	task "github.com/lhdhtrc/task-go/pkg"
 	"go.uber.org/zap"
 	"microservice-go/config"
 	"microservice-go/model"
 )
 
 type _CoreEntity struct {
+	ServiceDiscover map[string][]string
+	RemoteService   *model.RemoteServiceEntity
+
 	Config *config.CoreEntity
 	Logger *zap.Logger
 	Micro  *micro.CoreEntity
 	Etcd   *etcd.CoreEntity
-
-	ServiceDiscover map[string][]string
-	RemoteService   *model.RemoteServiceEntity
-
-	Task *tpg.CoreEntity
+	Task   *task.CoreEntity
 }
 
 var Use = new(_CoreEntity)
