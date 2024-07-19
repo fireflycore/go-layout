e0d51b8a1b86e7b136b51e9a09a7ca876d2aa46f
lhdht <15229684090@163.com>
2024-07-19 09:27:36 +0800

    update:config

--- config/core.go
+++ config/core.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	loger "github.com/lhdhtrc/logger-go/pkg"
+	micro "github.com/lhdhtrc/micro-go/pkg"
 	task "github.com/lhdhtrc/task-go/pkg"
 )
 
@@ -9,4 +10,5 @@ type CoreEntity struct {
 	System SystemConfigEntity `json:"system" bson:"system" yaml:"system" mapstructure:"system"`
 	Logger loger.ConfigEntity `json:"logger" bson:"logger" yaml:"logger" mapstructure:"logger"`
 	Task   task.ConfigEntity  `json:"task" bson:"task" yaml:"task" mapstructure:"task"`
+	Micro  micro.ConfigEntity `json:"micro" bson:"micro" yaml:"micro" mapstructure:"micro"`
 }
