54f832201910f2f1acb29f67632b96e91b916362
lhdht <15229684090@163.com>
2024-12-04 20:05:51 +0800

    fix: 废弃

--- api/core.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"google.golang.org/grpc"
-)
-
-func ServiceInstance(server *grpc.Server) {
-	// register microservice
-}
--- config/system.go
+++
@@ -1,5 +0,0 @@
-package config
-
-type SystemConfigEntity struct {
-	AppId string `json:"app_id" bson:"app_id" yaml:"app_id" mapstructure:"app_id"`
-}
