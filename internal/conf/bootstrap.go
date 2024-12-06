0987b2716ce134c33efca041d4f7b1362fc70705
lhdht <15229684090@163.com>
2024-12-06 22:12:33 +0800

    feature: 配置层

---
+++ internal/conf/bootstrap.go
@@ -0,0 +1,48 @@
+package conf
+
+import (
+	"encoding/json"
+	micro "github.com/lhdhtrc/micro-go/pkg"
+	task "github.com/lhdhtrc/task-go/pkg"
+	"os"
+	"path/filepath"
+)
+
+type BootstrapConf struct {
+	Server *ServerConf
+	Logger *LoggerConf
+
+	Micro *micro.ServiceConfig
+	Task  *task.Config
+}
+
+type ServerConf struct {
+	GrpcPort string `json:"grpc_port"`
+}
+
+type LoggerConf struct {
+	Remote  bool `json:"remote"`
+	Console bool `json:"console"`
+}
+
+func NewBootstrapConf() *BootstrapConf {
+	var bc BootstrapConf
+
+	cur, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	file := filepath.Join(cur, "conf", "bootstrap.json")
+
+	var b []byte
+	b, err = os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = json.Unmarshal(b, &bc); err != nil {
+		panic(err)
+	}
+
+	return &bc
+}
