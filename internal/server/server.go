ae022409900e29844956bc14c6b00a6e1502330d
lhdht <15229684090@163.com>
2024-12-06 22:09:58 +0800

    feature: tcp 服务

---
+++ internal/server/server.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"fmt"
+	"go-layout/internal/conf"
+	"net"
+)
+
+func NewServer(bc *conf.BootstrapConf) net.Listener {
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", bc.Server.GrpcPort))
+	if err != nil {
+		panic(err)
+	}
+
+	return listen
+}
