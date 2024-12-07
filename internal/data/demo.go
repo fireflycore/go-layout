0684962d026175d0f1108fb231d7eaf3c4a5c218
lhdht <15229684090@163.com>
2024-12-07 22:13:57 +0800

    fix: 修正模型位置

--- internal/data/demo.go
+++ internal/data/demo.go
@@ -3,10 +3,9 @@ package data
 import (
 	"context"
 	"github.com/lhdhtrc/func-go/object"
-	gorms "github.com/lhdhtrc/gorm/pkg"
+	"github.com/lhdhtrc/gorm/pkg"
 	pb "go-layout/dep/protobuf/gen/acme/demo/v1"
 	"go-layout/internal/biz"
-	"go-layout/model"
 )
 
 type demoRepo struct {
@@ -19,13 +18,13 @@ func NewDemoRepo(data *Data) biz.DemoRepo {
 	}
 }
 
-func (uc *demoRepo) Save(ctx context.Context, row *model.DemoEntity) error {
+func (uc *demoRepo) Save(ctx context.Context, row *biz.Demo) error {
 	uc.data.Mysql.WithContext(ctx).Create(&row)
 	return nil
 }
 
 func (uc *demoRepo) Update(ctx context.Context, request *pb.UpdateRequest) error {
-	var row model.DemoEntity
+	var row biz.Demo
 	if res := uc.data.Mysql.WithContext(ctx).Find(&row); res.Error != nil {
 		return res.Error
 	}
@@ -38,26 +37,26 @@ func (uc *demoRepo) Update(ctx context.Context, request *pb.UpdateRequest) error
 	return nil
 }
 
-func (uc *demoRepo) FindById(ctx context.Context, id string) (*model.DemoEntity, error) {
-	var row model.DemoEntity
+func (uc *demoRepo) FindById(ctx context.Context, id string) (*biz.Demo, error) {
+	var row biz.Demo
 	uc.data.Mysql.WithContext(ctx).Where("id = ?", id).Find(&row)
 	return &row, nil
 }
 
 func (uc *demoRepo) FindList(ctx context.Context, query *pb.GetListRequest) (int64, []*pb.Demo) {
-	sql := uc.data.Mysql.WithContext(ctx).Model(&model.DemoEntity{})
+	sql := uc.data.Mysql.WithContext(ctx).Model(&biz.Demo{})
 
 	var total int64
 	var list []*pb.Demo
 	sql.Count(&total)
 
-	gorms.WithPagingFilter(sql, query.Page, query.PageSize)
+	gorm.WithPagingFilter(sql, query.Page, query.PageSize)
 	sql.Find(&list)
 
 	return total, list
 }
 
 func (uc *demoRepo) DeleteById(ctx context.Context, id string) error {
-	uc.data.Mysql.WithContext(ctx).Where("id = ?", id).Delete(&model.DemoEntity{})
+	uc.data.Mysql.WithContext(ctx).Where("id = ?", id).Delete(&biz.Demo{})
 	return nil
 }
