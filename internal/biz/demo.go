9b555013476f5296b681f52b5953168f8212dd68
lhdht <15229684090@163.com>
2024-12-07 22:13:44 +0800

    fix: 修正模型位置

--- internal/biz/demo.go
+++ internal/biz/demo.go
@@ -2,14 +2,31 @@ package biz
 
 import (
 	"context"
+	gorm "github.com/lhdhtrc/gorm/pkg"
 	pb "go-layout/dep/protobuf/gen/acme/demo/v1"
-	"go-layout/model"
 )
 
+type Demo struct {
+	gorm.TableUUID
+
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+	Status      uint32 `json:"status"`
+	Sort        uint32 `json:"sort"`
+
+	AppId     string `json:"app_id" gorm:"size:36;index;"`
+	AccountId string `json:"account_id" gorm:"size:36;index;"`
+}
+
+func (Demo) Table() string {
+	return "demo"
+}
+
 type DemoRepo interface {
-	Save(ctx context.Context, row *model.DemoEntity) error
+	Save(ctx context.Context, row *Demo) error
 	Update(ctx context.Context, row *pb.UpdateRequest) error
-	FindById(ctx context.Context, id string) (*model.DemoEntity, error)
+	FindById(ctx context.Context, id string) (*Demo, error)
 	FindList(ctx context.Context, query *pb.GetListRequest) (int64, []*pb.Demo)
 	DeleteById(ctx context.Context, id string) error
 }
@@ -22,11 +39,11 @@ func NewDemoUseCase(repo DemoRepo) *DemoUseCase {
 	return &DemoUseCase{repo: repo}
 }
 
-func (uc *DemoUseCase) Save(ctx context.Context, row *model.DemoEntity) error {
+func (uc *DemoUseCase) Save(ctx context.Context, row *Demo) error {
 	return uc.repo.Save(ctx, row)
 }
 
-func (uc *DemoUseCase) FindById(ctx context.Context, id string) (*model.DemoEntity, error) {
+func (uc *DemoUseCase) FindById(ctx context.Context, id string) (*Demo, error) {
 	return uc.repo.FindById(ctx, id)
 }
 
