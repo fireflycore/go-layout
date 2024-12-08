69989771cfdc39cf0c224cb6c769c1b831a88ff2
lhdht <15229684090@163.com>
2024-12-08 20:21:22 +0800

    feature: 远程服务

---
+++ internal/service/remote.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	accessLogger "go-layout/dep/protobuf/gen/acme/logger/access/v1"
+	operationLogger "go-layout/dep/protobuf/gen/acme/logger/operation/v1"
+	serverLogger "go-layout/dep/protobuf/gen/acme/logger/server/v1"
+	"google.golang.org/grpc"
+)
+
+func NewAccessLoggerRemoteService(client *grpc.ClientConn) accessLogger.AccessLoggerServiceClient {
+	return accessLogger.NewAccessLoggerServiceClient(client)
+}
+
+func NewOperationLoggerRemoteService(client *grpc.ClientConn) operationLogger.OperationLoggerServiceClient {
+	return operationLogger.NewOperationLoggerServiceClient(client)
+}
+
+func NewServerLoggerRemoteService(client *grpc.ClientConn) serverLogger.ServerLoggerServiceClient {
+	return serverLogger.NewServerLoggerServiceClient(client)
+}
