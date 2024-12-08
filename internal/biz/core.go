4d592f5e35125cf70c3abd22c2c359dff31f0c78
lhdht <15229684090@163.com>
2024-12-08 20:25:30 +0800

    feature: logger接口

--- internal/biz/core.go
+++ internal/biz/core.go
@@ -4,3 +4,7 @@ import "github.com/google/wire"
 
 // ProviderSet is server providers.
 var ProviderSet = wire.NewSet(NewDemoUseCase)
+
+type AccessLogger func(b []byte, msg string)
+type ServerLogger func(b []byte)
+type OperationLogger func(b []byte)
