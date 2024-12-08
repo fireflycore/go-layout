302c0eaa0fb9ba617d58924d9ae38585f998414b
lhdht <15229684090@163.com>
2024-12-08 20:19:43 +0800

    feature: 访问日志中间件

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -1,15 +1,21 @@
 package server
 
 import (
+	"github.com/lhdhtrc/micro-go/pkg/middleware"
 	demo "go-layout/dep/protobuf/gen/acme/demo/v1"
+	"go-layout/internal/biz"
 	"go-layout/internal/service"
 	"google.golang.org/grpc"
 )
 
 func NewGrpcServer(
+	AccessLogger biz.AccessLogger,
+
 	Demo *service.DemoService,
 ) *grpc.Server {
-	srv := grpc.NewServer()
+	srv := grpc.NewServer(grpc.UnaryInterceptor(
+		middleware.GrpcAccessLogger(AccessLogger),
+	))
 
 	demo.RegisterDemoServiceServer(srv, Demo)
 
