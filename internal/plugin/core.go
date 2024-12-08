eddf2d7a90a9da2239842fbeacbd414f36e605c9
lhdht <15229684090@163.com>
2024-12-08 20:21:00 +0800

    feature: 注入grpc客户端及实现grpc客户单

--- internal/plugin/core.go
+++ internal/plugin/core.go
@@ -4,10 +4,30 @@ import (
 	"github.com/google/wire"
 	task "github.com/lhdhtrc/task-go/pkg"
 	"go-layout/internal/conf"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-var ProviderSet = wire.NewSet(NewTask)
+var ProviderSet = wire.NewSet(
+	NewTask,
+
+	NewGrpcClient,
+
+	NewAccessLogger,
+	NewServerLogger,
+	NewOperationLogger,
+)
 
 func NewTask(bc *conf.BootstrapConf) *task.Instance {
 	return task.New(bc.Task)
+}
+
+func NewGrpcClient(bc *conf.BootstrapConf) (*grpc.ClientConn, error) {
+	var addr string
+	if bc.Gateway.Network == bc.Micro.Network {
+		addr = bc.Gateway.InsideAddr
+	} else {
+		addr = bc.Gateway.OuterAddr
+	}
+	return grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 }
