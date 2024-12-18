a2bab1edad1ff0df5717aca748c49544e8a6d43b
lhdht <15229684090@163.com>
2024-12-18 20:41:09 +0800

    fix: 修改dto

--- internal/service/demo.dto.go
+++ internal/service/demo.dto.go
@@ -3,32 +3,14 @@ package service
 import (
 	pb "go-layout/dep/protobuf/gen/acme/demo/v1"
 	"go-layout/internal/biz"
-	"time"
 )
 
-func SaveDTO(src *pb.SaveRequest) *biz.Demo {
+func CreateDTO(src *pb.CreateRequest) *biz.Demo {
 	var dst biz.Demo
 	dst.Title = src.Title
 	dst.Description = src.Description
 	dst.Content = src.Content
 	dst.Status = src.Status
 	dst.Sort = src.Sort
-	return &dst
-}
-
-func GetDTO(src *biz.Demo) *pb.Demo {
-	var dst pb.Demo
-	dst.Id = src.ID
-	dst.Title = src.Title
-	dst.Description = src.Description
-	dst.Content = src.Content
-	dst.Status = src.Status
-	dst.Sort = src.Sort
-	dst.CreatedAt = src.CreatedAt.Format(time.DateTime)
-	dst.UpdatedAt = src.UpdatedAt.Format(time.DateTime)
-	dst.Author = &pb.Author{
-		Id:   src.AccountId,
-		Name: "",
-	}
 	return &dst
 }
