d541ae7c7a5b0918c53166576eb3df180cf91a49
lhdht <15229684090@163.com>
2025-04-15 17:54:52 +0800

    fix: 暂存

--- internal/conf/data.go
+++ internal/conf/data.go
@@ -3,7 +3,6 @@ package conf
 import (
 	etcd "github.com/lhdhtrc/etcd-go/pkg"
 	gorm "github.com/lhdhtrc/gorm/pkg"
-	micro "github.com/lhdhtrc/micro-go/pkg/core"
 	task "github.com/lhdhtrc/task-go/pkg"
 )
 
@@ -13,19 +12,7 @@ type DataConf struct {
 }
 
 func NewDataConf(bc *BootstrapConf, ist *task.Instance) *DataConf {
-	result := &DataConf{
-		Etcd:  &etcd.Config{},
-		Mysql: &gorm.Config{},
-	}
-	micro.ReadConfigTask(ist, bc.DataConfFile, []interface{}{
-		&result.Etcd,
-		&result.Mysql,
-	})
-	ist.Await()
-	micro.ReadCertAndWriteLocalTask(ist, "etcd", &result.Etcd.Tls)
-	micro.ReadCertAndWriteLocalTask(ist, "mysql", &result.Mysql.Tls)
-	ist.Await()
-
+	result := new(DataConf)
 	return result
 }
 
