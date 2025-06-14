46657493bcb8f7a826029d02fa4802fef275d964
lhdht <15229684090@163.com>
2025-06-14 21:04:32 +0800

    feature: biz示例代码

--- internal/biz/demo.go
+++ internal/biz/demo.go
@@ -2,37 +2,16 @@ package biz
 
 import (
 	"context"
-	gorm "github.com/lhdhtrc/gorm/pkg"
+	micro "github.com/lhdhtrc/micro-go/pkg/core"
 	pb "go-layout/dep/protobuf/gen/acme/demo/v1"
 )
 
-type Demo struct {
-	gorm.TableUUID
-
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Content     string `json:"content"`
-	Status      uint32 `json:"status"`
-	Sort        uint32 `json:"sort"`
-
-	AppId     string `json:"app_id" gorm:"size:36;index;"`
-	AccountId string `json:"account_id" gorm:"size:36;index;"`
-}
-
-func (Demo) Table() string {
-	return "demo"
-}
-
 type DemoRepo interface {
-	Create(ctx context.Context, row *Demo) error
-	Update(ctx context.Context, row *pb.UpdateRequest) error
+	Create(ctx context.Context, um *micro.UserContextMeta, request *pb.CreateRequest) error
+	FindList(ctx context.Context, um *micro.UserContextMeta, request *pb.FindListRequest) *pb.List
 	FindById(ctx context.Context, id string) (*pb.Demo, error)
-	FindList(ctx context.Context, request *pb.FindListRequest) *pb.List
-
-	DeleteById(ctx context.Context, id string)
-	DeleteByIds(ctx context.Context, ids []string)
-	DeleteByAppId(ctx context.Context, appId string)
-	DeleteByAccountId(ctx context.Context, accountId string)
+	Update(ctx context.Context, um *micro.UserContextMeta, request *pb.UpdateRequest) error
+	DeleteById(ctx context.Context, id string) error
 }
 
 type DemoUseCase struct {
@@ -43,22 +22,22 @@ func NewDemoUseCase(repo DemoRepo) *DemoUseCase {
 	return &DemoUseCase{repo: repo}
 }
 
-func (uc *DemoUseCase) Create(ctx context.Context, row *Demo) error {
-	return uc.repo.Create(ctx, row)
+func (uc *DemoUseCase) Create(ctx context.Context, um *micro.UserContextMeta, request *pb.CreateRequest) error {
+	return uc.repo.Create(ctx, um, request)
+}
+
+func (uc *DemoUseCase) FindList(ctx context.Context, um *micro.UserContextMeta, request *pb.FindListRequest) *pb.List {
+	return uc.repo.FindList(ctx, um, request)
 }
 
 func (uc *DemoUseCase) FindById(ctx context.Context, id string) (*pb.Demo, error) {
 	return uc.repo.FindById(ctx, id)
 }
 
-func (uc *DemoUseCase) FindList(ctx context.Context, request *pb.FindListRequest) *pb.List {
-	return uc.repo.FindList(ctx, request)
+func (uc *DemoUseCase) Update(ctx context.Context, um *micro.UserContextMeta, request *pb.UpdateRequest) error {
+	return uc.repo.Update(ctx, um, request)
 }
 
-func (uc *DemoUseCase) Update(ctx context.Context, row *pb.UpdateRequest) error {
-	return uc.repo.Update(ctx, row)
-}
-
-func (uc *DemoUseCase) DeleteById(ctx context.Context, id string) {
-	uc.repo.DeleteById(ctx, id)
+func (uc *DemoUseCase) DeleteById(ctx context.Context, id string) error {
+	return uc.repo.DeleteById(ctx, id)
 }
