b0cede3303d3a41bf0b3ce39232fd1e5b4bba164
lhdht <15229684090@163.com>
2025-06-14 21:07:48 +0800

    fix: 修改

--- internal/data/core.go
+++ internal/data/core.go
@@ -2,30 +2,11 @@ package data
 
 import (
 	"github.com/google/wire"
-	gorme "github.com/lhdhtrc/gorm/pkg"
-	"go-layout/internal/biz"
-	"go-layout/internal/conf"
-	"gorm.io/gorm"
+	"go-layout/dep/goverter/data"
 )
 
 var ProviderSet = wire.NewSet(NewMysql, NewData, NewDemoRepo)
 
-type Data struct {
-	db *gorm.DB
-}
-
-func NewMysql(bc *conf.BootstrapConf, mc *gorme.MysqlConf, logger biz.OperationLogger) (*gorme.MysqlDB, error) {
-	//mc.Conf.WithAutoMigrate(true)
-	mc.Conf.WithLoggerHandle(logger)
-	mc.Conf.WithLoggerConsole(bc.Logger.Console)
-
-	return gorme.NewMysql(mc, []interface{}{
-		&biz.Demo{},
-	})
-}
-
-func NewData(mysql *gorme.MysqlDB) (*Data, error) {
-	return &Data{
-		db: mysql.DB,
-	}, nil
-}
+var (
+	demoDTO = new(data.DemoConverterImpl)
+)
