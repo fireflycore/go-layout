4bb898fd921abfddd15e3d1d20d30b821571f2cf
lhdht <15229684090@163.com>
2025-06-14 21:06:32 +0800

    feature: dto转换方法

---
+++ internal/data/dto/utils.go
@@ -0,0 +1,33 @@
+package dto
+
+import (
+	"github.com/google/uuid"
+	gorm "github.com/lhdhtrc/gorm/pkg"
+	"time"
+)
+
+func TimeToString(t time.Time) string {
+	return t.Format(time.DateTime)
+}
+
+func StringToUUID(s string) gorm.BinUUID {
+	u, e := uuid.Parse(s)
+	if e != nil {
+		return gorm.BinUUID(uuid.Nil)
+	}
+	return gorm.BinUUID(u)
+}
+
+func UUIDToString(u gorm.BinUUID) string {
+	if uuid.UUID(u) == uuid.Nil {
+		return ""
+	}
+	return u.String()
+}
+
+func UUIDPtrToString(u *gorm.BinUUID) string {
+	if u != nil {
+		return u.String()
+	}
+	return ""
+}
