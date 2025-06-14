4a3da273a163780a36392440d270263ad2fd2131
lhdht <15229684090@163.com>
2025-06-14 21:04:55 +0800

    feature: 数据层示例代码

--- internal/data/demo.go
+++ internal/data/demo.go
@@ -2,10 +2,13 @@ package data
 
 import (
 	"context"
+	"errors"
 	"github.com/lhdhtrc/func-go/object"
 	"github.com/lhdhtrc/gorm/pkg"
+	micro "github.com/lhdhtrc/micro-go/pkg/core"
 	pb "go-layout/dep/protobuf/gen/acme/demo/v1"
 	"go-layout/internal/biz"
+	"go-layout/internal/data/entity"
 )
 
 type demoRepo struct {
@@ -18,57 +21,67 @@ func NewDemoRepo(data *Data) biz.DemoRepo {
 	}
 }
 
-func (uc *demoRepo) Create(ctx context.Context, row *biz.Demo) error {
-	uc.data.db.WithContext(ctx).Create(&row)
+func (uc *demoRepo) Create(ctx context.Context, um *micro.UserContextMeta, request *pb.CreateRequest) error {
+	row := demoDTO.ToCreate(request)
+	row.AppId = gorm.BinUUID(um.AppId)
+	row.UserId = gorm.BinUUID(um.UserId)
+	if res := uc.data.db.WithContext(ctx).Create(row); res.Error != nil {
+		return res.Error
+	}
 	return nil
 }
 
-func (uc *demoRepo) Update(ctx context.Context, request *pb.UpdateRequest) error {
-	var row biz.Demo
-	if res := uc.data.db.WithContext(ctx).Find(&row); res.Error != nil {
-		return res.Error
+func (uc *demoRepo) Update(ctx context.Context, _ *micro.UserContextMeta, request *pb.UpdateRequest) error {
+	old, err := uc.FindById(ctx, request.Id)
+	if err != nil {
+		return err
 	}
 
-	update := object.FilterChangeValue(&row, request, []string{"Id", "Type"})
-	if len(update) != 0 {
-		uc.data.db.WithContext(ctx).Updates(&row)
+	row := demoDTO.ToUpdate(request)
+
+	updates := object.FilterChangeValue(old, row, []string{})
+	if len(updates) == 0 {
+		return nil
+	}
+
+	if res := uc.data.db.WithContext(ctx).Model(&entity.Demo{}).Where("id = ?", request.Id).Updates(&updates); res.Error != nil {
+		return errors.New("更新失败")
 	}
 
 	return nil
 }
 
 func (uc *demoRepo) FindById(ctx context.Context, id string) (*pb.Demo, error) {
-	var row *pb.Demo
-	if res := uc.data.db.WithContext(ctx).Model(&biz.Demo{}).Where("id = ?", id).Find(&row); res.Error != nil {
+	var row entity.Demo
+	if res := uc.data.db.WithContext(ctx).Where("id = ?", id).Find(&row); res.Error != nil {
 		return nil, res.Error
 	}
-	return row, nil
+	return demoDTO.ToRow(&row), nil
 }
 
-func (uc *demoRepo) FindList(ctx context.Context, request *pb.FindListRequest) *pb.List {
-	var raw *pb.List
+func (uc *demoRepo) FindList(ctx context.Context, um *micro.UserContextMeta, request *pb.FindListRequest) *pb.List {
+	var (
+		raw  *pb.List
+		list []*entity.Demo
+	)
 
-	sql := uc.data.db.WithContext(ctx).Model(&biz.Demo{})
+	sql := uc.data.db.WithContext(ctx).Model(&entity.Demo{})
+	sql.Where("user_id = ?", um.UserId)
+	sql.Where("app_id = ?", um.AppId)
+	if len(request.SearchKey) != 0 {
+		sk := "%" + request.SearchKey + "%"
+		sql.Where("name LIKE ?", sk)
+	}
 	sql.Count(&raw.Total)
-
 	gorm.UsePaging(sql, request.Page, request.PageSize)
-	sql.Find(&raw.List)
+
+	sql.Find(&list)
+	raw.List = demoDTO.ToRaw(list)
 
 	return raw
 }
 
-func (uc *demoRepo) DeleteById(ctx context.Context, id string) {
-	uc.data.db.WithContext(ctx).Where("id = ?", id).Delete(&biz.Demo{})
-}
-
-func (uc *demoRepo) DeleteByIds(ctx context.Context, ids []string) {
-	uc.data.db.WithContext(ctx).Where("id IN ?", ids).Delete(&biz.Demo{})
-}
-
-func (uc *demoRepo) DeleteByAppId(ctx context.Context, appId string) {
-	uc.data.db.WithContext(ctx).Where("app_id = ?", appId).Delete(&biz.Demo{})
-}
-
-func (uc *demoRepo) DeleteByAccountId(ctx context.Context, accountId string) {
-	uc.data.db.WithContext(ctx).Where("account_id = ?", accountId).Delete(&biz.Demo{})
+func (uc *demoRepo) DeleteById(ctx context.Context, id string) error {
+	uc.data.db.WithContext(ctx).Where("id = UUID_TO_BIN(?)", id).Delete(&entity.Demo{})
+	return nil
 }
