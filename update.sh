53bea7ddf1298d44f967164af59491e9525f9f7f
lhdht <15229684090@163.com>
2025-10-07 21:26:57 +0800

    fix: 修改升级脚本

--- update.sh
+++ update.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-go get buf.build/go/protovalidate v1.0.0
-go get github.com/google/uuid v1.6.0
-go get github.com/google/wire v0.7.0
-go get github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.3
-go get github.com/lhdhtrc/compress-go v0.0.3
-go get github.com/lhdhtrc/crypto-go v0.0.3
-go get github.com/lhdhtrc/etcd-go v0.2.3
-go get github.com/lhdhtrc/func-go v0.3.0
-go get github.com/lhdhtrc/gorm v0.6.3
-go get github.com/lhdhtrc/logger-go v0.1.7
-go get github.com/lhdhtrc/micro-go v0.6.2
-go get go.etcd.io/etcd/client/v3 v3.6.5
-go get go.uber.org/zap v1.27.0
-go get google.golang.org/genproto/googleapis/api v0.0.0-20251006185510-65f7160b3a87
-go get google.golang.org/grpc v1.76.0
-go get google.golang.org/protobuf v1.36.10
-go get gorm.io/gorm v1.31.0
+go get buf.build/go/protovalidate@v1.0.0
+go get github.com/google/uuid@v1.6.0
+go get github.com/google/wire@v0.7.0
+go get github.com/grpc-ecosystem/grpc-gateway/v2@v2.27.3
+go get github.com/lhdhtrc/compress-go@v0.0.3
+go get github.com/lhdhtrc/crypto-go@v0.0.3
+go get github.com/lhdhtrc/etcd-go@v0.2.3
+go get github.com/lhdhtrc/func-go@v0.3.0
+go get github.com/lhdhtrc/gorm@v0.6.3
+go get github.com/lhdhtrc/logger-go@v0.1.7
+go get github.com/lhdhtrc/micro-go@v0.6.2
+go get go.etcd.io/etcd/client/v3@v3.6.5
+go get go.uber.org/zap@v1.27.0
+go get google.golang.org/genproto/googleapis/api@v0.0.0-20251006185510-65f7160b3a87
+go get google.golang.org/grpc@v1.76.0
+go get google.golang.org/protobuf@v1.36.10
+go get gorm.io/gorm@v1.31.0
