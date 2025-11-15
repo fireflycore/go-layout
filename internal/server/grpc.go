a9037e19a62c169c67c161fcf62002a4eb210aa7
lhdht <15229684090@163.com>
2025-11-15 17:07:33 +0800

    perfect: biz -> depend

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -3,13 +3,13 @@ package server
 import (
 	"github.com/lhdhtrc/micro-go/pkg/middleware"
 	demo "go-layout/dep/protobuf/gen/acme/demo/v1"
-	"go-layout/internal/biz"
+	"go-layout/internal/depend"
 	"go-layout/internal/service"
 	"google.golang.org/grpc"
 )
 
 func NewGrpcServer(
-	logger biz.AccessLogger,
+	logger depend.AccessLogger,
 
 	Demo *service.DemoService,
 ) *grpc.Server {
