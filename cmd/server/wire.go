f04d8f16ab200db1290ef543c8b2ba345c74f665
lhdht <15229684090@163.com>
2025-11-15 17:13:35 +0800

    perfect: define inject

--- cmd/server/wire.go
+++ cmd/server/wire.go
@@ -8,16 +8,18 @@ import (
 	"go-layout/internal/biz"
 	"go-layout/internal/conf"
 	"go-layout/internal/data"
-	"go-layout/internal/dep"
+	"go-layout/internal/depend"
+	"go-layout/internal/dto"
 	"go-layout/internal/server"
 	"go-layout/internal/service"
 )
 
 func wireApp(bc *conf.BootstrapConf) (*App, error) {
 	panic(wire.Build(
-		dep.ProviderSet,
+		depend.ProviderSet,
 		conf.ProviderSet,
 		data.ProviderSet,
+		dto.ProviderSet,
 		biz.ProviderSet,
 		service.ProviderSet,
 		server.ProviderSet,
