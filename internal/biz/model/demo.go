e4e1fd8cb8f4355253fe1a4a5feed3f6d71a9ffe
lhdht <15229684090@163.com>
2025-11-17 11:00:20 +0800

    feature: define UpdateDemo DO

---
+++ internal/biz/model/demo.go
@@ -0,0 +1,9 @@
+package biz
+
+type UpdateDemo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+	Status      uint32 `json:"status"`
+	Sort        uint32 `json:"sort"`
+}
