408d2469aa4ec8588fc6966486144b746fc327e9
lhdht <15229684090@163.com>
2025-11-23 11:40:41 +0800

    perfect: Load EtcdConf method

--- internal/conf/etcd.go
+++ internal/conf/etcd.go
@@ -1,41 +1,79 @@
 package conf
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	etcd "github.com/lhdhtrc/etcd-go/pkg"
-	config "go-layout/depend/protobuf/gen/acme/config/v1"
+	"go-layout/internal/biz/repo"
+	"time"
 )
 
-// EtcdLoader 实现 ConfigLoader 接口
-type EtcdLoader struct{}
+type EtcdConfLoader struct {
+	key   string
+	group string
 
-func NewEtcdConf(dc *Conf) *etcd.Config {
-	return dc.Etcd
+	bootstrapConf *BootstrapConf
+	configRepo    repo.ConfigRepo
+	utils         *Utils
 }
 
-func (ist *EtcdLoader) Local(dc *Conf) error {
-	filePath := getConfigFilePath("etcd.json")
+func NewEtcdConfLoader(bootstrapConf *BootstrapConf, utils *Utils, configRepo repo.ConfigRepo) *EtcdConfLoader {
+	return &EtcdConfLoader{
+		key:           "etcd",
+		group:         "database",
+		bootstrapConf: bootstrapConf,
+		configRepo:    configRepo,
+		utils:         utils,
+	}
+}
 
-	if err := loadJSONConfig(filePath, &dc.Etcd); err != nil {
-		return err
+func (load *EtcdConfLoader) Load() (*etcd.Conf, error) {
+	switch load.bootstrapConf.LoadConfMode {
+	case "local":
+		// 从本地加载
+		return load.Local()
+	case "remote":
+		// 从配置中心加载
+		return load.Remote()
+	default:
+		return nil, errors.New("not found load conf mode")
+	}
+}
+
+func (load *EtcdConfLoader) Local() (*etcd.Conf, error) {
+	var dst etcd.Conf
+
+	filePath := load.utils.GetConfigFilePath(fmt.Sprintf("%s.json", load.key))
+	if err := load.utils.LoadJSONConfig(filePath, &dst); err != nil {
+		return nil, err
 	}
 
-	return nil
+	return &dst, nil
 }
 
-func (ist *EtcdLoader) Remote(dc *Conf, bc *BootstrapConf, cc config.ConfigServiceClient) error {
-	content, err := fetchRemoteConfig(bc, cc, "database", "etcd")
+func (load *EtcdConfLoader) Remote() (*etcd.Conf, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
+	data, err := load.configRepo.GetConfig(ctx, load.bootstrapConf.AppId, load.group, load.key)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err = analyzeData(content, []byte(bc.AppSecret), &dc.Etcd); err != nil {
-		return err
+	var dst etcd.Conf
+
+	if err = load.utils.AnalyzeData(data.Content, []byte(load.bootstrapConf.AppSecret), &dst); err != nil {
+		return nil, err
 	}
 
-	if err = analyzeTlsData("etcd", dc.Etcd.Tls); err != nil {
-		return err
+	if err = load.utils.AnalyzeTlsData(load.key, dst.Tls); err != nil {
+		return nil, err
 	}
 
-	return nil
+	return &dst, nil
+}
+
+func NewEtcdConf(loader *EtcdConfLoader) (*etcd.Conf, error) {
+	return loader.Load()
 }
