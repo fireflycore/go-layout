2c405a7860f27e24925d09e0458e8ca4501205a6
lhdht <15229684090@163.com>
2025-11-23 11:42:45 +0800

    perfect: load MysqlConf method

--- internal/conf/mysql.go
+++ internal/conf/mysql.go
@@ -1,41 +1,80 @@
 package conf
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	gorm "github.com/lhdhtrc/gorm/pkg"
-	config "go-layout/depend/protobuf/gen/acme/config/v1"
+	"go-layout/internal/biz/repo"
+	"time"
 )
 
-// MysqlLoader 实现 ConfigLoader 接口
-type MysqlLoader struct{}
+type MysqlConfLoader struct {
+	key   string
+	group string
 
-func NewMysqlConf(dc *Conf) *gorm.MysqlConf {
-	return dc.Mysql
+	bootstrapConf *BootstrapConf
+	configRepo    repo.ConfigRepo
+	utils         *Utils
 }
 
-func (ist *MysqlLoader) Local(dc *Conf) error {
-	filePath := getConfigFilePath("mysql.json")
+func NewMysqlConfLoader(bootstrapConf *BootstrapConf, utils *Utils, configRepo repo.ConfigRepo) *MysqlConfLoader {
+	return &MysqlConfLoader{
+		key:           "mysql",
+		group:         "database",
+		bootstrapConf: bootstrapConf,
+		configRepo:    configRepo,
+		utils:         utils,
+	}
+}
 
-	if err := loadJSONConfig(filePath, &dc.Mysql.Conf); err != nil {
-		return err
+func (load *MysqlConfLoader) Load() (*gorm.MysqlConf, error) {
+	switch load.bootstrapConf.LoadConfMode {
+	case "local":
+		// 从本地加载
+		return load.Local()
+	case "remote":
+		// 从配置中心加载
+		return load.Remote()
+	default:
+		return nil, errors.New("not found load conf mode")
+	}
+}
+
+func (load *MysqlConfLoader) Local() (*gorm.MysqlConf, error) {
+	var dst gorm.MysqlConf
+
+	filePath := load.utils.GetConfigFilePath(fmt.Sprintf("%s.json", load.key))
+	if err := load.utils.LoadJSONConfig(filePath, &dst); err != nil {
+		return nil, err
 	}
 
-	return nil
+	return &dst, nil
 }
 
-func (ist *MysqlLoader) Remote(dc *Conf, bc *BootstrapConf, cc config.ConfigServiceClient) error {
-	content, err := fetchRemoteConfig(bc, cc, "database", "mysql")
+func (load *MysqlConfLoader) Remote() (*gorm.MysqlConf, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	data, err := load.configRepo.GetConfig(ctx, load.bootstrapConf.AppId, load.group, load.key)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err = analyzeData(content, []byte(bc.AppSecret), &dc.Mysql.Conf); err != nil {
-		return err
+	var dst gorm.MysqlConf
+
+	if err = load.utils.AnalyzeData(data.Content, []byte(load.bootstrapConf.AppSecret), &dst); err != nil {
+		return nil, err
 	}
 
-	if err = analyzeTlsData("mysql", dc.Mysql.Conf.Tls); err != nil {
-		return err
+	if err = load.utils.AnalyzeTlsData(load.key, dst.Tls); err != nil {
+		return nil, err
 	}
 
-	return nil
+	return &dst, nil
+}
+
+func NewPostgresConf(loader *MysqlConfLoader) (*gorm.MysqlConf, error) {
+	return loader.Load()
 }
