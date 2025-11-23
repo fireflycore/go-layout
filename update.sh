a3d06c0d8f056f425fd7b40b146c8be27339bfa0
lhdht <15229684090@163.com>
2025-11-23 11:44:57 +0800

    fix: update shell

--- update.sh
+++ update.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-go get buf.build/go/protovalidate@v1.0.0
+go get github.com/lhdhtrc/func-go@v0.3.1
+go get github.com/lhdhtrc/logger-go@v0.1.8
+go get github.com/lhdhtrc/micro-go@v0.6.5
+go get github.com/lhdhtrc/gorm@v0.7.3
+go get github.com/lhdhtrc/etcd-go@v0.2.4
+go get github.com/lhdhtrc/mongo-go@v0.5.2
+go get github.com/lhdhtrc/redis-go@v0.0.9
+
+go get buf.build/go/protovalidate@v1.0.1
 go get github.com/google/uuid@v1.6.0
 go get github.com/google/wire@v0.7.0
+go get github.com/grpc-ecosystem/go-grpc-middleware@v1.4.0
 go get github.com/grpc-ecosystem/grpc-gateway/v2@v2.27.3
-go get github.com/lhdhtrc/compress-go@v0.0.3
-go get github.com/lhdhtrc/crypto-go@v0.0.3
-go get github.com/lhdhtrc/etcd-go@v0.2.3
-go get github.com/lhdhtrc/func-go@v0.3.0
-go get github.com/lhdhtrc/gorm@v0.6.3
-go get github.com/lhdhtrc/logger-go@v0.1.7
-go get github.com/lhdhtrc/micro-go@v0.6.2
-go get go.etcd.io/etcd/client/v3@v3.6.5
-go get go.uber.org/zap@v1.27.0
-go get google.golang.org/genproto/googleapis/api@v0.0.0-20251006185510-65f7160b3a87
-go get google.golang.org/grpc@v1.76.0
+go get github.com/redis/go-redis/v9@v9.17.0
+go get go.etcd.io/etcd/client/v3@v3.6.6
+go get go.uber.org/zap@v1.27.1
+go get google.golang.org/genproto/googleapis/api@v0.0.0-20251111163417-95abcf5c77ba
+go get google.golang.org/grpc@v1.77.0
 go get google.golang.org/protobuf@v1.36.10
-go get gorm.io/gorm@v1.31.0
+go get gorm.io/datatypes@v1.2.7
+go get gorm.io/gorm@v1.31.1
