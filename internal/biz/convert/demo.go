f5c212d104edd3bff76c896731439b3795f647c6
lhdht <15229684090@163.com>
2025-11-26 20:07:06 +0800

    perfect: remove DemoConvert invalid interface

--- internal/biz/convert/demo.go
+++ internal/biz/convert/demo.go
@@ -2,23 +2,13 @@ package convert
 
 import (
 	pb "go-layout/dep/protobuf/gen/acme/demo/v1"
-	"go-layout/internal/biz/model"
 	"go-layout/internal/data/entity"
 )
 
 // goverter:converter
 // goverter:output:package :dto
 // goverter:output:file @cwd/dep/dto/demo.go
-// goverter:extend StringToUUID UUIDToString UUIDPtrToString TimeToString
 type DemoConvert interface {
 	// goverter:ignore TableUUID UserId AppId TenantId
 	ToCreate(row *pb.CreateDemoRequest) *entity.Demo
-	ToUpdate(row *pb.UpdateDemoRequest) *model.UpdateDemo
-
-	// goverter:ignore state unknownFields sizeCache Author
-	// goverter:map TableUUID.ID Id
-	// goverter:map TableUUID.CreatedAt CreatedAt
-	// goverter:map TableUUID.UpdatedAt UpdatedAt
-	ToRow(row *entity.Demo) *pb.Demo
-	ToRaw(raw []*entity.Demo) []*pb.Demo
 }
