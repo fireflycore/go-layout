6002255ddd867d678931afa1c311d526009430e0
lhdht <15229684090@163.com>
2025-11-26 20:13:09 +0800

    perfect: define demo biz model

--- internal/biz/model/demo.go
+++ internal/biz/model/demo.go
@@ -1,9 +1,21 @@
 package model
 
-type UpdateDemo struct {
+import (
+	gorm "github.com/lhdhtrc/gorm/pkg"
+)
+
+type Demo struct {
+	gorm.TableUUID
+
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Content     string `json:"content"`
 	Status      uint32 `json:"status"`
 	Sort        uint32 `json:"sort"`
+
+	UserId   string `json:"user_id"`
+	UserName string `json:"user_name"`
+
+	AppId    string `json:"app_id"`
+	TenantId string `json:"tenant_id"`
 }
