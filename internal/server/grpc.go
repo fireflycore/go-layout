e221cbab999ec89dcee5d398b2e4ea3a7dcea7f2
lhdht <15229684090@163.com>
2026-02-16 23:13:11 +0800

    perfect: use grpc middleware

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -2,22 +2,26 @@ package server
 
 import (
 	demo "go-layout/dep/protobuf/gen/acme/demo/v1"
+	"go-layout/internal/conf"
 	"go-layout/internal/dep"
 	"go-layout/internal/service"
 
-	"github.com/fireflycore/go-micro/middleware"
+	gm "github.com/fireflycore/go-micro/middleware/grpc"
 	ggm "github.com/grpc-ecosystem/go-grpc-middleware"
+	recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	"google.golang.org/grpc"
 )
 
 func NewGrpcServer(
 	logger dep.AccessLogger,
+	bootstrapConf *conf.BootstrapConf,
 
 	demoService *service.DemoService,
 ) *grpc.Server {
 	srv := grpc.NewServer(grpc.UnaryInterceptor(ggm.ChainUnaryServer(
-		middleware.PropagateIncomingMetadata,
-		middleware.GrpcAccessLogger(logger),
+		recovery.UnaryServerInterceptor(),
+		gm.NewServiceAccessLogger(logger),
+		gm.NewInjectServiceContext(bootstrapConf),
 	)))
 
 	demo.RegisterDemoServiceServer(srv, demoService)
