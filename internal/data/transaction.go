39ef980aa7c382d39008484a5e49f125d5bb7375
lhdht <15229684090@163.com>
2026-04-23 07:44:13 +0800

    perfect: tx

--- internal/data/transaction.go
+++ internal/data/transaction.go
@@ -3,19 +3,21 @@ package data
 import (
 	"context"
 	"go-layout/internal/biz/repo"
+
 	"gorm.io/gorm"
 )
 
 type transaction struct {
-	db *gorm.DB
+	data *Data
 }
 
-func NewTransaction(db *gorm.DB) repo.Transaction {
-	return &transaction{db: db}
+func NewTransaction(data *Data) repo.Transaction {
+	return &transaction{data: data}
 }
 
-func (t *transaction) ExecTx(ctx context.Context, fn func(ctx context.Context) error) error {
-	return t.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+// ExecTx 在同一个数据库事务中执行回调，并把当前事务句柄透传给下游 Repo。
+func (r *transaction) ExecTx(ctx context.Context, fn func(ctx context.Context) error) error {
+	return r.data.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
 		// 将事务DB放入context，让Repo方法能够获取到
 		txCtx := context.WithValue(ctx, repo.TransactionContextKey, tx)
 		return fn(txCtx)
