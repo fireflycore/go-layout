f3e5d2faa0fac0fb45fffa7583e2fc47be5c2a92
lhdht <15229684090@163.com>
2026-04-22 22:01:03 +0800

    fix: inject related dept

--- internal/dep/core.go
+++ internal/dep/core.go
@@ -1,23 +1,26 @@
 package dep
 
 import (
-	"github.com/fireflycore/go-micro/rpc"
+	"github.com/fireflycore/go-micro/logger"
 	"github.com/fireflycore/go-micro/sys"
+	"github.com/fireflycore/go-micro/telemetry"
 	"github.com/fireflycore/go-utils/compress"
 	"github.com/fireflycore/go-utils/crypto"
 	"github.com/google/wire"
 )
 
+// ProviderSet 组装模板库基础依赖，包括 telemetry、标准日志和 invocation 调用能力。
 var ProviderSet = wire.NewSet(
 	crypto.NewAESCrypto,
 	compress.NewGZIP,
 	sys.NewHostInfo,
 
-	rpc.NewRemoteInvokeServiceContext,
-	rpc.NewRemoteServiceGrpcClient,
+	telemetry.NewProviders,
 
-	NewLogger,
-	NewAccessLogger,
-	NewServerLogger,
-	NewOperationLogger,
+	logger.NewZapLogger,
+	logger.NewAccessLogger,
+	logger.NewServerLogger,
+
+	NewInvocationConnectionManager,
+	NewUnaryInvoker,
 )
