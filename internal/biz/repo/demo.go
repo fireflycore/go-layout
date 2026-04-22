81b90426218c33481cf31179b73fff0e00225c4f
lhdht <15229684090@163.com>
2026-04-22 22:31:07 +0800

    fix: rpc -> invocation

--- internal/biz/repo/demo.go
+++ internal/biz/repo/demo.go
@@ -5,12 +5,12 @@ import (
 	pb "go-layout/dep/protobuf/gen/acme/demo/v1"
 	"go-layout/internal/data/entity"
 
-	"github.com/fireflycore/go-micro/rpc"
+	"github.com/fireflycore/go-micro/invocation"
 )
 
 type DemoRepo interface {
 	CreateDemo(ctx context.Context, row *entity.Demo) error
-	GetDemoList(ctx context.Context, um *rpc.UserContextMeta, request *pb.GetDemoListRequest) *pb.DemoList
+	GetDemoList(ctx context.Context, um *invocation.UserContextMeta, request *pb.GetDemoListRequest) *pb.DemoList
 	GetDemoInfo(ctx context.Context, id string) (*pb.Demo, error)
 	UpdateDemo(ctx context.Context, id string, row map[string]interface{}) error
 	DeleteDemo(ctx context.Context, id string) error
