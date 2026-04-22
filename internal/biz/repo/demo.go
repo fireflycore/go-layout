d53b3b17ab8be2cf6dd2d9c102f5f58ed49aabbe
lhdht <15229684090@163.com>
2026-04-23 07:07:34 +0800

    fix: type

--- internal/biz/repo/demo.go
+++ internal/biz/repo/demo.go
@@ -12,7 +12,7 @@ type DemoRepo interface {
 	CreateDemo(ctx context.Context, row *entity.Demo) error
 	GetDemoList(ctx context.Context, um *invocation.UserContextMeta, request *pb.GetDemoListRequest) *pb.DemoList
 	GetDemoInfo(ctx context.Context, id string) (*pb.Demo, error)
-	UpdateDemo(ctx context.Context, id string, row map[string]interface{}) error
+	UpdateDemo(ctx context.Context, id string, row map[string]any) error
 	DeleteDemo(ctx context.Context, id string) error
 	GetDemoCount(ctx context.Context, status *uint32) int64
 }
