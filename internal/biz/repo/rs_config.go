eccacc77ab34ba5392d84562fe585db800a5d1b6
lhdht <15229684090@163.com>
2026-04-23 07:07:56 +0800

    fix: abandoned

--- internal/biz/repo/rs_config.go
+++
@@ -1,9 +0,0 @@
-package repo
-
-import (
-	pb "go-layout/dep/protobuf/gen/acme/config/v1"
-)
-
-type ConfigRepo interface {
-	GetConfig(appId, group, key string) (*pb.Config, error)
-}
--- internal/biz/repo/rs_logger.go
+++
@@ -1,7 +0,0 @@
-package repo
-
-type LoggerRepo interface {
-	CreateAccessLog(appId string, raw []byte)
-	CreateServerLog(appId string, raw []byte)
-	CreateOperationLog(appId string, raw []byte)
-}
