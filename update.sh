7d845b47a62f6bc7d581622b59d49a8757240476
lhdht <15229684090@163.com>
2026-05-05 09:52:11 +0800

    feature: update shell

--- update.sh
+++ update.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-go get github.com/fireflycore/go-micro@v0.7.0
-go get github.com/fireflycore/go-logger@v0.2.1
-go get github.com/fireflycore/go-utils@v0.3.5
-go get github.com/fireflycore/go-etcd@v0.2.6
-go get github.com/fireflycore/go-redis@v0.1.1
-go get github.com/fireflycore/gormx@v0.8.1
+go get github.com/fireflycore/go-consul@v0.1.7
+go get github.com/fireflycore/go-micro@v1.5.1
+go get github.com/fireflycore/go-redis@v0.1.2
+go get github.com/fireflycore/go-utils@v0.3.6
+go get github.com/fireflycore/gormx@v1.0.0
 
-go get buf.build/go/protovalidate@v1.0.1
+go get buf.build/go/protovalidate@v1.1.3
 go get github.com/google/uuid@v1.6.0
 go get github.com/google/wire@v0.7.0
 go get github.com/grpc-ecosystem/go-grpc-middleware@v1.4.0
-go get github.com/grpc-ecosystem/grpc-gateway/v2@v2.27.3
+go get github.com/grpc-ecosystem/grpc-gateway/v2@v2.28.0
+go get github.com/hashicorp/consul/api@v1.31.2
 go get github.com/redis/go-redis/v9@v9.17.1
-go get go.etcd.io/etcd/client/v3@v3.6.6
+go get go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc@v0.67.0
 go get go.uber.org/zap@v1.27.1
-go get google.golang.org/genproto/googleapis/api@v0.0.0-20251124214823-79d6a2a48846
-go get google.golang.org/grpc@v1.77.0
-go get google.golang.org/protobuf@v1.36.10
+go get google.golang.org/genproto/googleapis/api@v0.0.0-20260311181403-84a4fc48630c
+go get google.golang.org/grpc@v1.79.2
+go get google.golang.org/protobuf@v1.36.11
 go get gorm.io/datatypes@v1.2.7
 go get gorm.io/gorm@v1.31.1
