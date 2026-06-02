f4b56f8b691bc3a0099ece9e4584257041a27972
lhdht <15229684090@163.com>
2026-06-02 16:00:47 +0800

    update:shell

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-go get github.com/fireflycore/go-consul@v0.2.9
-go get github.com/fireflycore/go-micro@v1.5.9
+go get github.com/fireflycore/go-consul@v0.3.1
+go get github.com/fireflycore/go-micro@v1.6.1
 go get github.com/fireflycore/go-redis@v0.1.3
 go get github.com/fireflycore/go-utils@v0.3.7
 go get github.com/fireflycore/gormx@v1.0.0
