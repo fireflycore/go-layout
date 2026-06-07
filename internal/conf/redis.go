c0f53eb7f912719c01813e054ecd6be7dfab2e81
lhdht <15229684090@163.com>
2026-06-07 11:10:09 +0800

    perfect: code style

--- internal/conf/redis.go
+++ internal/conf/redis.go
@@ -8,7 +8,7 @@ import (
 	"github.com/fireflycore/go-redis"
 )
 
-// NewRedisConfig 从 Consul Store 读取当前生效的 Redis 配置。
+// NewRedisConfig 从数据面 Store 读取当前生效的 Redis 配置。
 func NewRedisConfig(bootstrapConfig *BootstrapConfig, utils *Utils, store config.Store) (*redis.Config, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
